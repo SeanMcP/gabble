2eacc06aec1e50449ce3498d48777b6c5c2c53bc
Sean McPherson <seanmcp25@gmail.com>
2017-09-01 15:20:31 -0400

    Add 'liked by' users to individual gabs; limit likes to one per user

--- models/user.js
+++ models/user.js
@@ -9,7 +9,7 @@ module.exports = function(sequelize, DataTypes) {
 
   User.associate = function(models) {
     User.hasMany(models.Post, { as: "posts", foreignKey: 'userId' })
-    
+
     User.hasMany(models.Like, { as: "likes", foreignKey: 'userId' })
   }
 
--- routes/index.js
+++ routes/index.js
@@ -68,13 +68,13 @@ router.post('/signup', function(req, res) {
 
 router.get('/user', isAuthenticated, function(req, res) {
   models.Post.findAll({
-    include: [{
-      model: models.User,
-      as: 'user'
-    }]
+    order: [['createdAt', 'DESC']],
+    include: [
+      { model: models.User, as: 'user' },
+      { model: models.Like, as: 'likes' }
+    ]
   })
   .then(function(data) {
-    console.log('data.user:\n', data.user)
     res.render('user', { data: data })
   })
 })
@@ -85,7 +85,6 @@ router.get('/logout', function(req, res) {
 })
 
 router.post('/create', function(req, res) {
-  console.log('req.user.id: ', req.user.id)
   models.Post.create({
     userId: req.user.id,
     content: req.body.content,
@@ -98,16 +97,29 @@ router.post('/create', function(req, res) {
 })
 
 router.get('/gab/:id', function(req, res) {
-  console.log('req.params.id: ', req.params.id);
-  models.Post.findOne({ where: { id: req.params.id } })
+
+  models.Post.findOne({
+    where: { id: req.params.id },
+    include: [
+      { model: models.User, as: 'user' },
+      { model: models.Like, as: 'likes' }
+    ]
+  })
   .then(function(data) {
-    console.log('DATA:\n', data);
-    console.log('data.userId: ', data.userId, '\n', 'req.user.id: ', req.user.id)
-    if (data.userId == req.user.id) {
-      res.render('gab', { data: data, delete: true })
-    } else {
-      res.render('gab', { data: data })
-    }
+    let arr = [];
+    data.likes.forEach(function(like){ arr.push(like.userId) })
+    console.log('arr', arr);
+    models.User.findAll({ where: { id: arr } })
+    .then(function(users) {
+      console.log('users: ', users)
+      res.render('gab', { data: data, users: users })
+    })
+    // User.findAll()
+    // if (data.userId == req.user.id) {
+    //   res.render('gab', { data: data, delete: true })
+    // } else {
+    //   res.render('gab', { data: data })
+    // }
   })
   .catch(function(err) {
     res.send(err)
@@ -121,6 +133,29 @@ router.get('/delete/:id', function(req, res) {
     }
   })
   .then(function(data) {
+    res.redirect('/user')
+  })
+})
+
+router.get('/like/:postId', function(req, res) {
+  models.Like.findAll({ where: { postId: req.params.postId } })
+  .then(function(data) {
+
+    let arr = []
+
+    data.forEach(function(user) {
+      arr.push(user.userId)
+    })
+
+    if (!arr.includes(req.user.id)) {
+      models.Like.create({
+        userId: req.user.id,
+        postId: req.params.postId,
+        createdAt: Date.now(),
+        updatedAt: Date.now()
+      })
+    }
+
     res.redirect('/user')
   })
 })
